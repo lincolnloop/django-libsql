38c5f248e60654340abf2b0f857e292cf60c53fb
Peter Baumgartner <pete@lincolnloop.com>
2023-10-03 14:18:40 -0600

    Avoid using Django's default test db name

--- .github/workflows/libsql_settings.py
+++ .github/workflows/libsql_settings.py
@@ -4,17 +4,27 @@ import uuid
 DATABASES = {
     "default": {
         "ENGINE": "django_libsql",
-        "NAME": "/tmp/test.db",
+        "NAME": "/tmp/default.db",
         "SYNC_URL": os.environ["LIBSQL_SYNC_URL"],
         "AUTH_TOKEN": os.environ["LIBSQL_AUTH_TOKEN"],
-        "TEST": {"SYNC_URL": "test" + os.environ["LIBSQL_SYNC_URL"]},
+        "TEST": {
+            # libsql does not like django's default in-memory database name
+            # file:memorydb_default?mode=memory&cache=shared
+            "NAME": "/tmp/testdefault.db",
+            "SYNC_URL": "test" + os.environ["LIBSQL_SYNC_URL"]
+        },
     },
     "other": {
         "ENGINE": "django_libsql",
-        "NAME": "/tmp/test.db",
+        "NAME": "/tmp/other.db",
         "SYNC_URL": os.environ["OTHER_LIBSQL_SYNC_URL"],
         "AUTH_TOKEN": os.environ["LIBSQL_AUTH_TOKEN"],
-        "TEST": {"SYNC_URL": "test" + os.environ["OTHER_LIBSQL_SYNC_URL"]},
+        "TEST": {
+            # libsql does not like django's default in-memory database name
+            # file:memorydb_default?mode=memory&cache=shared
+            "NAME": "/tmp/testother.db",
+            "SYNC_URL": "test" + os.environ["OTHER_LIBSQL_SYNC_URL"]
+        },
     },
 }
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
